327fdd014848aac3f05b5008875683a77b145126
Pez Cuckow <email@pezcuckow.com>
2021-04-21 20:14:37 +0200

    ✨ Generate an index.html

--- README.md
+++ README.md
@@ -2,9 +2,9 @@
 
 High level usage:
 
- - Fork this repo
- - Add gifs to ./gifs
- - Tweak ./gifs/categories.json
- - Check `yarn run build` works locally
- - Enable github pages for the `gh-pages` command
- - Profit!
+- Fork this repo
+- Add gifs to ./gifs
+- Tweak ./gifs/categories.json
+- Check `yarn run build` works locally
+- Enable github pages for the `gh-pages` command
+- Profit!
--- script/build.js
+++ script/build.js
@@ -1,49 +1,68 @@
 #!/usr/bin/env node
 
 import { promises as fs } from 'fs'
-import { join, extname } from 'path'
-
+import { join, extname, basename } from 'path'
 
 import logging from 'debug-level'
 
+import glob from 'glob-promise'
+import Handlebars from 'handlebars'
+import HandlebarsHelpers from 'handlebars-helpers'
 import JSON5 from 'json5'
 
 import { loadGifs } from './gifs.js'
+import { groupBy } from './utils.js'
 
 const log = logging.log('custom-gifs:build')
 
 const GIFS_PATH = './gifs'
 const BUILD_PATH = './build'
+const WEB_PATH = './web'
 
 const GIFS_FULL_PATH = join(process.cwd(), GIFS_PATH)
 const BUILD_FULL_PATH = join(process.cwd(), BUILD_PATH)
 
+const VIEWS_FULL_PATH = join(process.cwd(), WEB_PATH, './views')
+const PUBLIC_FULL_PATH = join(process.cwd(), WEB_PATH, './public')
+
 /// Helpers
 
-async function copyDir(src, dest) {
+async function copyDir(src, dest, extensionsToCopy, root = false) {
+  if (root) {
+    log.info(
+      `Copying files matching ${extensionsToCopy.join(
+        ', ',
+      )} from ${src} to ${dest}`,
+    )
+  } else {
+    log.debug(
+      `Copying files matching ${extensionsToCopy.join(
+        ', ',
+      )} from ${src} to ${dest}`,
+    )
+  }
+
   await fs.mkdir(dest, { recursive: true })
   const entries = await fs.readdir(src, { withFileTypes: true })
-
-  const extensionsToCopy = ['.gif']
-
-  log.debug(
-    `Copying files matching ${extensionsToCopy.join(
-      ', ',
-    )} from ${src} to ${dest}`,
-  )
 
   for (const entry of entries) {
     const srcPath = join(src, entry.name)
     const destPath = join(dest, entry.name)
 
-    const extension = extname(entry.name).toLowerCase()
-
     if (entry.isDirectory()) {
-      await copyDir(srcPath, destPath)
-    } else if (extensionsToCopy.includes(extension)) {
-      await fs.copyFile(srcPath, destPath)
+      await copyDir(srcPath, destPath, extensionsToCopy)
     } else {
-      log.debug(`Ignoring ${entry.name}`)
+      const extension = extname(entry.name).toLowerCase()
+
+      const matchesExtension = extensionsToCopy.length
+        ? extensionsToCopy.includes(extension)
+        : true
+
+      if (matchesExtension) {
+        await fs.copyFile(srcPath, destPath)
+      } else {
+        log.debug(`Ignoring ${entry.name}`)
+      }
     }
   }
 }
@@ -68,10 +87,48 @@ async function generateMetaFile(gifDirectory, buildDirectory) {
 
   log.debug(`Full meta file`, meta)
 
-  await fs.writeFile(
-    join(buildDirectory, 'meta.json'),
-    JSON.stringify(meta, null, 2),
-  )
+  const metaPath = join(buildDirectory, 'meta.json')
+  await fs.writeFile(metaPath, JSON.stringify(meta, null, 2))
+  log.info(`Meta file written to ${metaPath}`)
+
+  return meta
+}
+
+async function generateIndex(viewsDirectory, buildDirectory, gifsInfo) {
+  const { gifs } = gifsInfo
+
+  Handlebars.registerHelper(HandlebarsHelpers(['string']))
+
+  const indexHBSPath = join(viewsDirectory, 'index.handlebars')
+  const partialsPath = join(viewsDirectory, 'partials/')
+  const templateString = await fs.readFile(indexHBSPath)
+
+  const partials = await glob('**/*.handlebars', { root: partialsPath })
+  for (const partial of partials) {
+    const partialName = basename(partial).split('.')[0]
+
+    log.info(`Registered partial ${partialName} to ${partial}`)
+
+    const partialTemplate = await fs.readFile(partial)
+
+    Handlebars.registerPartial(partialName, partialTemplate.toString());
+  }
+
+  const template = Handlebars.compile(templateString.toString(), { noEscape: true })
+
+  const byCategory = groupBy(gifs, 'category')
+  Object.keys(byCategory).forEach((key) => {
+    byCategory[key] = groupBy(byCategory[key], 'subcategory')
+  })
+  const output = template({
+    categories: byCategory,
+  })
+
+  const indexPath = join(buildDirectory, 'index.html')
+
+  await fs.writeFile(indexPath, output)
+
+  log.info(`Index page written to ${indexPath}`)
 }
 
 /// Main
@@ -79,13 +136,13 @@ async function generateMetaFile(gifDirectory, buildDirectory) {
 async function main() {
   log.info(`Building using gifs in ${GIFS_FULL_PATH}`)
 
-  await copyDir(GIFS_FULL_PATH, BUILD_FULL_PATH)
+  await copyDir(GIFS_FULL_PATH, BUILD_FULL_PATH, ['.gif'], true)
+  const gifsInfo = await generateMetaFile(GIFS_FULL_PATH, BUILD_FULL_PATH)
 
-  log.info(`Copied all gifs from ${GIFS_FULL_PATH} to ${BUILD_FULL_PATH}`)
+  await copyDir(PUBLIC_FULL_PATH, BUILD_FULL_PATH, [], true)
+  await generateIndex(VIEWS_FULL_PATH, BUILD_FULL_PATH, gifsInfo)
 
-  await generateMetaFile(GIFS_FULL_PATH, BUILD_FULL_PATH)
-
-  log.info(`Meta file written to ${BUILD_FULL_PATH}`)
+  log.info(`Build complete, output in ${BUILD_FULL_PATH}`)
 }
 
 main()
